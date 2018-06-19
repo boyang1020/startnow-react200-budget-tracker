58a592d178078b2d966f876b2be7b73887d887f1
boyang1020 <boyang1020@gmail.com>
2018-06-19 14:20:51 -0700

    start now-react200-budget-tracker

--- src/js/components/ExpenseEntries/expenseReducer.js
+++ src/js/components/ExpenseEntries/expenseReducer.js
@@ -1,9 +1,39 @@
-const defaultState ={
+import { REFUSED } from "dns";
+
+const defaultState = {
     description: '',
     amount: '',
     lineItems: []
 };
 
-export default function ExpenseReducer (state = defaultState, action) {
-    return state;
+export default function ExpenseReducer(state = defaultState, action) {
+    const { type, payload } = action,
+    switch (type) {
+        case 'UPDATE_EXPENSE_DESCRIPTION': {
+            return {
+                ...state,
+                description: payload.description
+            };
+        }
+        case 'UPDATE_EXPENSE_AMOUNT': {
+            return {
+                ...state,
+                amount: payload.amount
+            };
+        }
+        case 'ADD_EXPENSE': {
+            return {
+                description: '',
+                action: '',
+                lineItems: [
+                    ...state.lineItems,
+                    { description, amount }
+                ]
+
+            };
+        }
+        default: {
+            return state;
+        }
+    }
 }
--- src/js/components/IncomeEntries/incomeReducer.js
+++ src/js/components/IncomeEntries/incomeReducer.js
@@ -1,9 +1,39 @@
-const defaultState ={
+import { REFUSED } from "dns";
+
+const defaultState = {
     description: '',
     amount: '',
     lineItems: []
 };
 
-export default function IncomeReducer (state =defaultState, action) {
-    return state;
+export default function INCOMEReducer(state = defaultState, action) {
+    const { type, payload } = action,
+    switch (type) {
+        case 'UPDATE_INCOME_DESCRIPTION': {
+            return {
+                ...state,
+                description: payload.description
+            };
+        }
+        case 'UPDATE_INCOME_AMOUNT': {
+            return {
+                ...state,
+                amount: payload.amount
+            };
+        }
+        case 'ADD_INCOME': {
+            return {
+                description: '',
+                action: '',
+                lineItems: [
+                    ...state.lineItems,
+                    { description, amount }
+                ]
+
+            };
+        }
+        default: {
+            return state;
+        }
+    }
 }
